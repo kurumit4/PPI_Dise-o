78a0a4bd54ec1a80e688eec0ef5754ee96bc0886
kurumit4 <samuel.idrobo@correo.tdea.edu.co>
2026-03-17 15:04:43 -0500

    actualizacion de tailwind y correcciones

---
+++ frontend/src/pages/Menu_Helados.jsx
@@ -0,0 +1,85 @@
+
+export default function Menu_Helados() {
+  return (
+
+    <>
+    
+        <header>
+        <h1>Mimos</h1>
+        <p>Soporte | Catálogo | Carrito |</p>
+    </header>
+ 
+    <hr>
+     </hr>
+ 
+    <h2>Menú</h2>
+    <hr>
+     </hr>
+    <section>
+ 
+        <h3>Copa Euforia</h3>
+        <p>[Imagen Copa Euforia]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+        <br></br>
+ 
+        <h3>Copa Brownie</h3>
+        <p>[Imagen Copa Brownie]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+        <br></br>
+ 
+        <h3>Copa Banana</h3>
+        <p>[Imagen Copa Banana]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+    </section>
+ 
+    <hr>
+     </hr>
+    <section>
+ 
+        <h3>Copa Tentación</h3>
+        <p>[Imagen Copa Tentación]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+        <br></br>
+ 
+        <h3>Copa Oblea</h3>
+        <p>[Imagen Copa Oblea]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+        <br></br>
+ 
+        <h3>Copa Granolado</h3>
+        <p>[Imagen Copa Granolado]</p>
+        <p>Precio: $11.900</p>
+        <button>Agregar</button>
+        <button>Quitar</button>
+ 
+    </section>
+ 
+    <hr>
+     </hr> 
+        <footer>
+        <p>Convenios | Encuéntranos | Trabaja con nosotros | Contacto | Políticas</p>
+    </footer>
+    
+    
+    
+    
+    
+    </>
+  );
+  
+}
